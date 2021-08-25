4513de3eaaf80718fa86352ca50dbd3cc2a75674
Mark Lagodych <lgd.mrk@gmail.com>
2021-08-25 23:08:55 +0300

    Removed unneeded parentheses

--- xmake.lua
+++ xmake.lua
@@ -1,21 +1,26 @@
-set_xmakever("2.5.0")
-set_project("Fejix")
-set_version("0.0.0")
+set_xmakever "2.5.0"
+set_project "Fejix"
+set_version "0.0.0"
 
-includes("include/backends/backends.lua")
+includes "include/backends/backends.lua"
 
 add_backend_requires()
 
-target("fejix")
-    set_kind("static")
 
-    add_files("src/*.c")
-    add_includedirs("include")
+target "fejix"
+    set_kind "static"
+
+    add_files "src/*.c"
+    add_includedirs "include"
     
     add_backend()
 
-target("fejix_test")
-    set_kind("binary")
-    add_files("src/test.c")
-    add_includedirs("include")
-    add_deps("fejix")
+
+target "fejix_test"
+
+    set_kind "binary"
+
+    add_files "src/test.c"
+    add_includedirs "include"
+
+    add_deps "fejix"
